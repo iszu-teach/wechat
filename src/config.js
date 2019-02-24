35ff84a021ff48d574ab9839e5181131b36a703a
Nicholas <32389766+NicholasCao@users.noreply.github.com>
2019-02-24 23:55:00 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,15 +1,14 @@
 'use strict'
-
 const path = require('path')
 
 module.exports = {
-  db: 'mongodb://localhost/',
-  log: path.resolve(__dirname, '../logs/wechat.log'),
+  db: 'mongodb://szuwechat:szu0408@localhost:28018/',
+  log: path.resolve(__dirname, '../../logs/wechat.log'),
   base: 'wechat',
   port: '3001',
-  root: 'http://iszu.cn',
-  secret: 'iszu666',
-  appID: '',
-  appSecret: '',
-  wechatTokenCache: path.resolve(__dirname, '../')
+  root: 'https://szuwechat.cn',
+  secret: 'szu0408',
+  appID: 'wx44bb58ea4fb385a7',
+  appSecret: 'ec6441eff9c6ae3ca909c4a9bf34ea30',
+  wechatTokenCache: path.resolve(__dirname, '../../')
 }
